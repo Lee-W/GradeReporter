6fde7af4e1a406807b9512f10ffb6941ea390071
LeeW <cl87654321@gmail.com>
2017-12-29 17:47:33 +0800

    Fix GPACalaulator result error

--- src/main/java/GPACalculator.java
+++ src/main/java/GPACalculator.java
@@ -10,14 +10,19 @@ public abstract class GPACalculator {
         double creditSum = 0;
 
         double grade, credit;
+
         for (int i=0; i<grades.size(); i++) {
             grade = grades.get(i);
             credit = credits.get(i);
 
             gpa += translateGradeToGPA(grade)*credit;
+            creditSum += credit;
         }
-
-        return 0;
+        if (creditSum != 0) {
+            return gpa/creditSum;
+        } else {
+            return 0;
+        }
     }
 
     protected abstract double translateGradeToGPA(double grade);
